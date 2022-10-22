99546148d7578b8241ba91d1cc0b6134e1732a02
Nhi Nguyen <minhnhi08061105@gmail.com>
2022-10-22 17:03:07 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './CsutomDirective/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, SetBackgroundDirective],
+  declarations: [AppComponent, SetBackgroundDirective, HighlightDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
