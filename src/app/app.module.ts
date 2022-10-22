5a1adbb108caac40233df28ff96f49adfb58797f
Nhi Nguyen <minhnhi08061105@gmail.com>
2022-10-22 21:57:05 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './CsutomDirective/highlight.directive';
 import { HoverDirective } from './CsutomDirective/hover.directive';
+import { BetterhighlightDirective } from './CsutomDirective/betterhighlight.directive';
+import { ClassDirective } from './CsutomDirective/class.directive';
+import { StyleDirective } from './CsutomDirective/style.directive';
 
 @NgModule({
-  declarations: [AppComponent, SetBackgroundDirective, HighlightDirective, HoverDirective],
+  declarations: [AppComponent, SetBackgroundDirective, HighlightDirective, HoverDirective, BetterhighlightDirective, ClassDirective, StyleDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
